52e3d84c7b7d3e8c85a8c0382337ce10d018e705
ElizaTest <eliza.radius@gmail.com>
2019-09-06 10:10:07 +0500

    ;;;;;;;;

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,6 +7,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("write URL:");
         Scanner MyVar = new Scanner(System.in);
+        System.out.println("write name of note");
         System.out.println(MyVar.nextLine());
     }
 }
